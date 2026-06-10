9dfb94827d83af1fde9aae5b7e28ec01dc006ecd
Gabriel <gabrielcostafil1@gmail.com>
2026-06-10 11:38:17 -0300

    adiciona enum de permissões Role

--- app-tarefas-auth/src/types/session.d.ts
+++ app-tarefas-auth/src/types/session.d.ts
@@ -1,13 +1,11 @@
-// ============================================================
-// 🎯 TODO 1: Tipar a session
-// ============================================================
-
 import "express-session";
+import { Role } from "../enums/Role";
 
 declare module "express-session" {
   interface SessionData {
     userId: number;
     userName: string;
+    userRole: Role;
     flash: string | null;
   }
 }
