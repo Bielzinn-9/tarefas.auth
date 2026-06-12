af553d258931ed4bd95684d0f9702d09a2e9f6d6
Gabriel <gabrielcostafil1@gmail.com>
2026-06-12 13:40:43 -0300

    registra rotas administrativas

--- app-tarefas-auth/src/app.ts
+++ app-tarefas-auth/src/app.ts
@@ -3,6 +3,7 @@ import session from "express-session";
 
 import { authRoutes } from "./routes/authRoutes";
 import { tarefaRoutes } from "./routes/tarefaRoutes";
+import { adminRoutes } from "./routes/adminRoutes";
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static("public"));
 
 app.use(authRoutes);
 app.use(tarefaRoutes);
+app.use(adminRoutes);
 
 app.listen(3000, () =>
     console.log("✅ App Tarefas rodando em http://localhost:3000")
